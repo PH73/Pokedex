efe43610e202b16244160de01cddb4779ee04e83
Paul <Paul@macmini.local>
2017-06-21 20:06:26 +0100

    Alamofire and CocoaPods installed and built

--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -11,7 +11,18 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
-
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     @IBOutlet weak var nameLbl: UILabel!
     
     override func viewDidLoad() {
@@ -22,5 +33,10 @@ class PokemonDetailVC: UIViewController {
     }
 
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        // the function below returns the screen to the home view (first screen)
+        dismiss(animated: true, completion: nil)
+        
+    }
 
 }
